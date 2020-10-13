93c9dd4e04830c018d587e51a35caaef84320a43
Rizwan Hashmi <mri.hashmi@gmail.com>
2020-10-12 22:53:29 -0400

    Update App.js
    Added displaying message. and some fun

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ export default function App() {
   
   let [count, setCount] = useState(0)
   let [isMorning, setMorning] = useState(false)
+  //let [colorname, setMorning] = useState(false)
 
   return (
     <div className = {`box ${isMorning ? 'dayLight' : ''}`}>
       <h1> Good {isMorning ? 'Morning' : 'Night'}</h1>
+      <h1> The color is {isMorning ? 'Yellow' : 'Dark'}</h1>
 
       <Message counter={count}/>
       <br/>
@@ -17,10 +19,15 @@ export default function App() {
       <button onClick={
         ()=> setCount(count + 1)
         }>
-         Update counter
+         Click here to Update the counter
+
+         <br/>
       </button>
-    
-      <button onClick = {()=>setMorning(!isMorning)}> Update Day</button>
+      <br/><br/>
+      <button onClick = {()=>setMorning(!isMorning)}> Update Day Light</button>
+      <h1> {count == 4 ? 'Stop Playing with the button ;-)' : ''}</h1>
+      <h1> {count >= 6 ? 'Hey! I told you to Stop :-O' : ''}</h1>
+      {/* <h1>{()=> count = 3 ('stop')} </h1> */}
     </div>
   );
 }
